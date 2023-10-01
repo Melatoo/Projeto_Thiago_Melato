d93968aea2942a967c56bbb9bcf29c87787eaed2
Thiago Melato <thiagim9@gmail.com>
2023-10-01 15:32:29 -0300

    update b

--- B.java
+++ B.java
@@ -1,4 +1,3 @@
-
 public class B {
     private int B1;
     private float B2;
@@ -27,33 +26,3 @@ public class B {
         System.out.println("MB2");
     }
 }
-
-public class B {
-    private int B1;
-    private float B2;
-
-    public int getB1() {
-        return B1;
-    }
-
-    public float B2() {
-        return B2;
-    }
-
-    public void setB1(int B1) {
-        this.B1 = B1;
-    }
-
-    public void setB2(float B2) {
-        this.B2 = B2;
-    }
-
-    public void MB1() {
-        System.out.println("MB1");
-    }
-
-    public void MB2() {
-        System.out.println("MB2");
-    }
-}
->>>>>>> 7b38b20b92cf3ddfe6408dc648b44c023407967e
